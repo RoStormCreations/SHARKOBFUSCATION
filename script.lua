69c3bf8b5a73aead543be29d5c1c630593d0cb6d
re_sistance <rostormstudios@gmail.com>
2024-12-07 11:14:33 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RoStormCreations/Shark/refs/heads/main/Gamelist.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RoStormCreations/Shark/refs/heads/main/miscellaneous/gamelist.lua"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
